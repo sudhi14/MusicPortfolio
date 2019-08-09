da30fe058ac651710da4614a1dfcddcd74ad5f16
Sudheep S <sudheep.1426@gmail.com>
2019-08-09 11:32:53 -0400

    Update Firebase.js

--- src/DataProvider/StoreData/Firebase.js
+++ src/DataProvider/StoreData/Firebase.js
@@ -5,13 +5,7 @@ import 'firebase/auth'
 
 const firebaseBackEnd = () => {
     const firebaseConfig = {
-        apiKey: "AIzaSyA_ZXK6cTXoZposHVkhTCcR7-Xs0YLwwUs",
-        authDomain: "myproject-ea169.firebaseapp.com",
-        databaseURL: "https://myproject-ea169.firebaseio.com",
-        projectId: "myproject-ea169",
-        storageBucket: "myproject-ea169.appspot.com",
-        messagingSenderId: "395229971218",
-        appId: "1:395229971218:web:eca7f961e18caebe"
+        // firebase cinfiguration
       };
 
       if (!firebase.apps.length) {
